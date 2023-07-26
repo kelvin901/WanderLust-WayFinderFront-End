1f5466a4a51fd569ffd8be448f81ea1340eeca6f
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-07-26 22:44:12 +0300

    Raw Login/Signup

---
+++ src/components/LoginForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here (e.g., send data to the backend API for user registration)
+    console.log('Form data submitted:', formData);
+    // Reset the form after submission
+    setFormData({
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
