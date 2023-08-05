4e1c158627928eb33f72ca9870b2cd41edddaddf
gorety <wagutugorety@gmail.com>
2023-08-05 15:01:10 +0300

    delete unused component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,6 @@ import Front from "../components/Home/Front";
 // import Funds from "../components/Home/Funds";
 import HowItWorks from "../components/Home/HowItWorks";
 // import News from "../components/Home/News";
-import Quiz from "../components/Home/Quiz";
 import Stats from "../components/Home/Stats";
 import Stories from "../components/Home/Stories";
 import Trusted from "../components/Home/Trusted";
@@ -17,7 +16,6 @@ export default function Home() {
       <Stats />
       {/* <Funds /> */}
       <AttractionsGrid/>
-      <Quiz />
       {/* <Discover /> */}
       <HowItWorks />
       {/* <News /> */}
