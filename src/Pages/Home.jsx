82b08fbabd514480a40e4171c7f3d196e83b0825
gorety <wagutugorety@gmail.com>
2023-08-05 15:11:41 +0300

    delete unused pictures

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,5 @@
-// import Discover from "../components/Home/Explore";
 import Front from "../components/Home/Front";
-// import Funds from "../components/Home/Funds";
 import HowItWorks from "../components/Home/HowItWorks";
-// import News from "../components/Home/News";
 import Stats from "../components/Home/Stats";
 import Stories from "../components/Home/Stories";
 import Trusted from "../components/Home/Trusted";
@@ -14,11 +11,8 @@ export default function Home() {
     <main>
       <Front />
       <Stats />
-      {/* <Funds /> */}
       <AttractionsGrid/>
-      {/* <Discover /> */}
       <HowItWorks />
-      {/* <News /> */}
       <Stories />
       <Trusted />
     </main>
