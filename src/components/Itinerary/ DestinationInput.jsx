41fe50e77e856687ceed0461366e3884c40f58ad
alvinkenyagah <alvin47k@gmail.com>
2023-08-08 16:58:35 +0300

    Add sweet alert form popup to add destination

--- src/components/Itinerary/ DestinationInput.jsx
+++ src/components/Itinerary/ DestinationInput.jsx
@@ -1,124 +1,83 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAuth } from '../../AuthContext';
+import Swal from 'sweetalert2'; // Import SweetAlert
 
 const DestinationInput = () => {
-  const { user } = useAuth(); // Get the logged-in user from the AuthContext
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [interests, setInterests] = useState('');
-  const [budget, setBudget] = useState('');
-  const [location, setLocation] = useState('');
+  const { user } = useAuth();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Perform a fetch request to save the destination data to the server
-    // Include the user_id from the logged-in user
-    fetch('/destinations', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name,
-        description,
-        interests,
-        budget,
-        location,
-        user_id: user.id, // Include the user_id from the logged-in user
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response, you can show a success message or redirect
-        console.log('Destination saved:', data);
-      })
-      .catch((error) => {
-        // Handle errors
-        console.error('Error saving destination:', error);
-      });
-  };
+
+  const handleSubmit = () => {
+    Swal.fire({
+      title: 'Add a Destination',
+      html: `
+      <form id="destination-form">
+      <input type="text" id="swal-input-name" class="swal2-input" placeholder="Destination Name" required>
+      <input id="swal-input-description" class="swal2-input" placeholder="Description" required>
+      <input type="text" id="swal-input-location" class="swal2-input" placeholder="Location" required>
+    </form>
+      `,
+      showCancelButton: true,
+      confirmButtonText: 'Save Destination',
+      preConfirm: () => {
+        const swalName = document.getElementById('swal-input-name').value;
+        const swalDescription = document.getElementById('swal-input-description').value;
+        const swalLocation = document.getElementById('swal-input-location').value;
+        
+        // Replace this with your actual fetch logic
+        fetch('/destinations', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: swalName,
+            description: swalDescription,
+            location: swalLocation,
+            user_id: user.id,
+          }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.id) {
+            Swal.fire({
+              title: 'Success',
+              text: 'Destination saved successfully!',
+              icon: 'success',
+            });
+          } else {
+            Swal.fire({
+              title: 'Error',
+              text: 'Error saving destination. Please try again.',
+              icon: 'error',
+            });
+          }
+        })
+        .catch((error) => {
+          console.error('Error saving destination:', error);
+        });
+    },
+  });
+};
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h2 className="text-2xl font-bold mb-4">Add a Destination</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-            Destination Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="border border-gray-400 p-2 w-full"
-            placeholder="Enter destination name"
-          />
+      <form onSubmit={(event) => event.preventDefault()} className="flex flex-wrap -mx-4">
+        <div className="w-full px-4 flex justify-center">
+          <button
+            type="button"
+            onClick={handleSubmit} // Open the SweetAlert form on button click
+            className="bg-blue-500 text-white px-2 w-1/4 py-1 rounded"
+          >
+            Add Destination
+          </button>
         </div>
-
-        <div className="mb-4">
-          <label htmlFor="description" className="block text-sm font-medium text-gray-700">
-            Description
-          </label>
-          <textarea
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="border border-gray-400 p-2 w-full"
-            placeholder="Enter description"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="interests" className="block text-sm font-medium text-gray-700">
-            Interests
-          </label>
-          <textarea
-            id="interests"
-            value={interests}
-            onChange={(e) => setInterests(e.target.value)}
-            className="border border-gray-400 p-2 w-full"
-            placeholder="Enter interests"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="budget" className="block text-sm font-medium text-gray-700">
-            Budget
-          </label>
-          <input
-            type="number"
-            id="budget"
-            value={budget}
-            onChange={(e) => setBudget(e.target.value)}
-            className="border border-gray-400 p-2 w-full"
-            placeholder="Enter budget"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="location" className="block text-sm font-medium text-gray-700">
-            Location
-          </label>
-          <input
-            type="text"
-            id="location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            className="border border-gray-400 p-2 w-full"
-            placeholder="Enter location"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Save Destination
-        </button>
       </form>
     </div>
   );
 };
 
 export default DestinationInput;
+
+
+
